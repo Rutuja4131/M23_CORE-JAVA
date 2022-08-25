01112e766332d953072f80ec3a6bd8f3d519bde0
pankaj4131 <pankajmanjare516@gmail.com>
2022-08-25 20:59:32 +0530

    first commit

---
+++ Day5_Inheritance,Scanner and BufferedReader class/src/com/cg/inheritance/SingleInheritance.java
@@ -0,0 +1,28 @@
+package com.cg.inheritance;
+// Program on single inheritance
+
+class Parent
+{
+	protected int rollno;
+	void print()
+	{
+		System.out.println("Hey Guys !,Whats going on...?");
+	}
+}
+class Child extends Parent
+{
+	void accept()
+	{
+		System.out.println("Roll No is : "+rollno);
+	}
+}
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.rollno=121;
+		c.accept();
+		c.print();
+	}
+
+}
